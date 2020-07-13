4701000ea09b92fa070ef5458199c461b614e091
Antonio Camargo <antoniop.camargo@gmail.com>
2020-07-13 19:34:06 -0300

    Fix

--- .github/workflows/build_website.sh
+++ .github/workflows/build_website.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Install yarn and build website
-yarn global add vuepress@1.5.2 vuepress-plugin-md-enhance@0.7.0
+yarn global add vuepress@1.5.2 vuepress-plugin-md-enhance@0.6.10
 yarn --cwd ./website website:build
