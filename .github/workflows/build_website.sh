4f63dd1737be5be147176b81343cd5bc4fdd4b59
Antonio Camargo <antoniop.camargo@gmail.com>
2020-08-07 09:28:59 -0300

    Update vuepress

--- .github/workflows/build_website.sh
+++ .github/workflows/build_website.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Install yarn and build website
-yarn global add vuepress@1.5.2
+yarn global add vuepress@1.5.3
 yarn add -D maginapp/vuepress-plugin-katex
 yarn --cwd ./website website:build
