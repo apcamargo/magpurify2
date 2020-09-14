7a2a89c37ebd296c030a7b3a9e1ff7675fe7b21c
Antonio Camargo <antoniop.camargo@gmail.com>
2020-09-14 03:07:24 -0300

    Chenge formula again

--- magpurify2/core.py
+++ magpurify2/core.py
@@ -451,7 +451,7 @@ class ContigClassifier:
                         "for the genomes not found in it."
                     )
                 threshold_dict = {
-                    genome: 0.066 - 0.0125 * (1 - np.exp(1) ** (0.037 * score))
+                    genome: 0.07 - 0.0125 * (1 - np.exp(1) ** (0.037 * score))
                     for genome, score in checkm_score_dict.items()
                 }
                 self.probability_threshold = np.array(
@@ -461,7 +461,7 @@ class ContigClassifier:
                     ]
                 )
                 self.probability_threshold = np.clip(
-                    self.probability_threshold, 0.06, 0.4
+                    self.probability_threshold, 0.08, 0.4
                 )
             else:
                 self.probability_threshold = probability_threshold
