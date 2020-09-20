a637de20fcb254e9f01286696a4a361b14271135
Antonio Camargo <antoniop.camargo@gmail.com>
2020-09-20 13:35:44 -0300

    Fix genome_min_fraction range

--- magpurify2/modules/taxonomy.py
+++ magpurify2/modules/taxonomy.py
@@ -35,7 +35,7 @@ def main(args):
         args.contig_min_fraction, "contig_min_fraction", [0.5, 1.0]
     )
     args.genome_min_fraction = tools.validade_input(
-        args.genome_min_fraction, "genome_min_fraction", [0.5, 1.0]
+        args.genome_min_fraction, "genome_min_fraction", [0.01, 0.99]
     )
     args.genome_min_fraction = tools.validade_input(
         args.min_genus_identity, "min_genus_identity", [0.0, 1.0]
