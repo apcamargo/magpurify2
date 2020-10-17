c2edca5b5a7d0de102fbb4e7f388f757cf7458f4
Antonio Camargo <antoniop.camargo@gmail.com>
2020-10-17 16:52:02 -0300

    Stricter

--- magpurify2/core.py
+++ magpurify2/core.py
@@ -552,7 +552,7 @@ class ContigClassifier:
             self.mags_contaminants_dict[genome][contig] = contaminant
 
     def get_dynamic_thresholds(self, checkm_scores):
-        f = lambda score: 0.15 + 0.35 * (np.exp(0.014 * score) - 1)
+        f = lambda score: 0.15 + 0.35 * (np.exp(0.013 * score) - 1)
         threshold_dict = {genome: f(score) for genome, score in checkm_scores.items()}
         probability_threshold = np.array(
             [
